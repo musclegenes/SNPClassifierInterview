c0a974852dff50da24022b53e943808065790577
Simon-Fitnessgenes <Simon.burns@fitnessgenes.com>
2021-02-07 14:30:36 +0000

    update with some comments to help interviewees understand the task better. Added some packages which may be of use during the test

--- SNPClassifier/Controllers/ClassificationController.cs
+++ SNPClassifier/Controllers/ClassificationController.cs
@@ -14,12 +14,24 @@ namespace SNPClassifier.Controllers
         }
 
 
+        /// <summary>
+        /// The result of the task should be returned here.
+        /// The result model should be placed inside the View constructor
+        /// </summary>
+        /// <returns>List<SnpClassificationViewModel></returns>
         public IActionResult Index()
         {
+            //Model return intentionally left blank.
+            //This action will nto return a view without error until a model has been passed.
             return View();
         }
 
 
+        /// <summary>
+        /// Example action which uses a randomly generated set of profiles and classifications to show the
+        /// resulting output of the classification view 
+        /// </summary>
+        /// <returns>List<SnpClassificationViewModel></returns>
         public IActionResult Example()
         {
             var model = new List<SnpClassificationViewModel>();
